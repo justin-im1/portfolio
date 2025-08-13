9bd377507b5f21ca0144d57b86c828e9ef922535
justin24Codes <imjustin339@gmail.com>
2025-08-13 00:02:34 -0400

    remove unused font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,6 @@
 import type { Metadata } from "next";
-import { Oxanium, Electrolize } from "next/font/google";
+import { Electrolize } from "next/font/google";
 import "./globals.css";
-
-// const oxanium = Oxanium({
-//   subsets: ["latin"],
-//   display: 'swap',
-//   weight: ['400', '700'],
-// });
 
 const electrolize = Electrolize({
   weight: ['400'],
