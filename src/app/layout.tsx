950948575a408d94c1ce712aa0da15d27ac04d33
justin24Codes <imjustin339@gmail.com>
2025-08-13 00:00:53 -0400

    fix lint errors

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,11 +2,11 @@ import type { Metadata } from "next";
 import { Oxanium, Electrolize } from "next/font/google";
 import "./globals.css";
 
-const oxanium = Oxanium({
-  subsets: ["latin"],
-  display: 'swap',
-  weight: ['400', '700'],
-});
+// const oxanium = Oxanium({
+//   subsets: ["latin"],
+//   display: 'swap',
+//   weight: ['400', '700'],
+// });
 
 const electrolize = Electrolize({
   weight: ['400'],
--- src/components/sections/AboutSection.tsx
+++ src/components/sections/AboutSection.tsx
@@ -2,15 +2,15 @@ export default function AboutSection() {
   return (
     <div className="text-white/90 text-sm md:text-base leading-loose space-y-6">
       <p>
-        Hi 👋 I'm a Systems Design Engineering student at the University of Waterloo, specializing in full-stack development, driven by a passion for building impactful software solutions. I'm always striving to improve and committed to continuous learning. In previous internships at Constant Contact, University Health Network, and Voxer, I've built a wide range of powerful features for users.
+        Hi 👋 I&apos;m a Systems Design Engineering student at the University of Waterloo, specializing in full-stack development, driven by a passion for building impactful software solutions. I&apos;m always striving to improve and committed to continuous learning. In previous internships at Constant Contact, University Health Network, and Voxer, I&apos;ve built a wide range of powerful features for users.
       </p>
       
       <p>
-        Outside of coding, I'm a die-hard Toronto Maple Leafs and Toronto Blue Jays fan. I also love golfing, with Torrey Pines South being my favourite course, and experimenting with new recipes from around the world.
+        Outside of coding, I&apos;m a die-hard Toronto Maple Leafs and Toronto Blue Jays fan. I also love golfing, with Torrey Pines South being my favorite course, and experimenting with new recipes from around the world.
       </p>
 
       <p>
-        I'm currently seeking winter 2026 SWE internship opportunities!
+        I&apos;m currently seeking Winter 2026 SWE internship opportunities!
       </p>
     </div>
   );
